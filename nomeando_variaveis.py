1d1caed4434c7751f91aa938fc92e72f186274b8
Gabriel Mendes <gabrielomendes02@gmail.com>
2024-04-30 14:36:40 -0300

    Aprendendo a nomear variáveis

---
+++ nomeando_variaveis.py
@@ -0,0 +1,22 @@
+Laranja, Melão, Limao = 1, 2, 3
+print(Laranja, Melão, Limao)
+
+Morango = Uva = Kiwi = 100
+print(Morango, Uva, Kiwi)
+
+# Nomear com listas
+Lista_Carros = ['vw', 'Porsche', 'Hyundai']
+Marca1, Marca2, Marca3 = Lista_Carros
+print(Marca1, Marca2, Marca3)
+
+# Combinando variáveis
+Nome = 'João'
+Sobrenome = 'Silva'
+Nome_Completo = Nome + ' ' + Sobrenome
+print(Nome_Completo)
+
+# Experimento
+Nome = 'João'
+idade = str(30)
+
+print(Nome, idade + " " + "anos")
